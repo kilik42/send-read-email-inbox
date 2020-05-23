312851cf15a4b00dc412d34fa1a6670ccc73baea
marvin evins <maevins@cps.edu>
2020-05-22 23:14:59 -0500

    working with templating the multipart email

--- send_email.py
+++ send_email.py
@@ -1,4 +1,6 @@
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 username = 'test@gmail.com'
 password = 'letsgetitstarted2020'
@@ -6,9 +8,13 @@ password = 'letsgetitstarted2020'
 
 def send_mail(text='Email Body', subject ='Hello world', from_email='test@gmail.com', to_emails=None):
     assert isinstance(to_emails, list)
+    msg = MIMEMultipart('alternative')
+    msg['From'] = from_email
+    msg['To'] = to_emails
+
 
     #log in to smtp server
-    server = smtplib.SMTP()
+    server = smtplib.SMTP(host='smtp.sendgrid.com', port=547)
     server.ehlo()
     server.starttls()
     server.login()
